b1ca8a1711bb62f23a050b98864a07d11c09014a
Manobendra mondal <manobendramondal316@gmail.com>
2026-02-12 22:21:01 -0800

    Update app.py

--- app.py
+++ app.py
@@ -133,6 +133,19 @@ with tab1:
             else:
                 st.error("Failed to update expense")
 
+=================================================================================
+import streamlit as st
+from add_update_ui import add_update_tab
+from analytics_ui import analytics_tab
+st.title("Expense Tracking System")
+
+tab1, tab2 = st.tabs(["Add/Updates", "Analytics"])
+
+with tab1:
+    add_update_tab()
+with tab2:
+    analytics_tab()
+
 
 
 
